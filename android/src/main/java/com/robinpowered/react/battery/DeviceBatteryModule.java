7f320a40d186dc568a72c4fe14c99c528fbd256b
Atticus White <contact@atticuswhite.com>
2015-12-01 21:44:58 -0500

    android implementation

---
+++ android/src/main/java/com/robinpowered/react/battery/DeviceBatteryModule.java
@@ -0,0 +1,79 @@
+package com.robinpowered.react.battery;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.os.BatteryManager;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+// @link http://developer.android.com/training/monitoring-device-state/battery-monitoring.html
+public class DeviceBatteryModule extends ReactContextBaseJavaModule {
+  public static final String EVENT_NAME = "batteryChange";
+  public static final String IS_CHARGING_KEY = "charging";
+  public static final String BATTERY_LEVEL_KEY = "level";
+
+  private ReactApplicationContext reactApplicationContext;
+  private Activity activity = null;
+  private Intent batteryStatus = null;
+  private BroadcastReceiver batteryStateReceiver;
+
+
+  public DeviceBatteryModule(ReactApplicationContext reactApplicationContext, Activity activity) {
+    super(reactApplicationContext);
+    this.reactApplicationContext = reactApplicationContext;
+    this.activity = activity;
+    this.registerBatteryStateReceiver();
+  }
+
+  private float getBatteryPrecentageFromIntent(Intent intent) {
+    int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+    int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    return level / (float) scale;
+  }
+
+  private boolean getIsChangingFromIntent(Intent intent) {
+    int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+    return (
+      status == BatteryManager.BATTERY_STATUS_CHARGING ||
+      status == BatteryManager.BATTERY_STATUS_FULL
+    );
+  }
+
+  private WritableNativeMap getJSMap (Intent intent) {
+    float batteryPercentage = getBatteryPrecentageFromIntent(intent);
+    boolean isCharging = getIsChangingFromIntent(intent);
+    System.out.println("BATTERY LEVEL CHANGE DETECTED");
+    WritableNativeMap params = new WritableNativeMap();
+    params.putBoolean(IS_CHARGING_KEY, isCharging);
+    params.putDouble(BATTERY_LEVEL_KEY, (double) batteryPercentage);
+    return params;
+  }
+
+  private void registerBatteryStateReceiver() {
+    batteryStateReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        WritableNativeMap params = getJSMap(intent);
+        reactApplicationContext
+          .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+          .emit(EVENT_NAME, params);
+      }
+    };
+    IntentFilter batteryFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+    batteryStatus = this.activity.registerReceiver(batteryStateReceiver, batteryFilter);
+  }
+
+  @Override
+  public String getName() {
+    return "DeviceBattery";
+  }
+}
---
+++ android/src/main/java/com/robinpowered/react/battery/DeviceBatteryPackage.java
@@ -0,0 +1,39 @@
+package com.robinpowered.react.battery;
+
+import android.app.Activity;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DeviceBatteryPackage implements ReactPackage {
+  private Activity mActivity = null;
+
+  public DeviceBatteryPackage(Activity activity) {
+    mActivity = activity;
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+    List<NativeModule> modules = new ArrayList<NativeModule>();
+    modules.add(new DeviceBatteryModule(reactApplicationContext, mActivity));
+    return modules;
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+    return Arrays.<ViewManager>asList();
+  }
+}
