f9835fca9648de707fd12fc95be359c1b31bd3aa
Tom Sella <tsella@wework.com>
2017-02-08 14:33:07 -0500

    1. support rn 0.40+ (backward compat implemented but not tested) 2. use EventEmitter instead of deprecated EventDispatcher 3. fix duplicate includes in DeviceBattery.m

--- ios/DeviceBattery/DeviceBattery.h
+++ ios/DeviceBattery/DeviceBattery.h
@@ -5,13 +5,21 @@
 //  Created by Atticus White on 12/1/15.
 //  Copyright © 2015 Atticus White. All rights reserved.
 //
-
+//  2017/02/08 @tsella
+//  - support rn 0.40+
+//  - use EventEmitter instead of deprecated EventDispatcher
 
 #import <Foundation/Foundation.h>
+#if __has_include(<React/RCTAssert.h>)
 #import <React/RCTBridgeModule.h>
 #import <React/RCTBridge.h>
-#import <React/RCTEventDispatcher.h>
+#import <React/RCTEventEmitter.h>
+#else // backward compat for rn pre 0.40
+#import "RCTBridgeModule.h"
+#import "RCTBridge.h"
+#import "RCTEventEmitter.h"
+#endif
 
-@interface DeviceBattery : NSObject<RCTBridgeModule>
+@interface DeviceBattery : RCTEventEmitter <RCTBridgeModule>
 
 @end
