6b708dc976dfb100816d549e7115bca9fb059a6b
Alexander Tseung <alextsg@users.noreply.github.com>
2018-02-28 23:24:57 -0800

    Add removeListener function

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const batteryEventEmitter = new NativeEventEmitter(DeviceBattery);
 export default {
   isCharging: DeviceBattery.isCharging,
   getBatteryLevel: DeviceBattery.getBatteryLevel,
-  addListener: callback => batteryEventEmitter.addListener(DeviceBattery.BATTERY_CHANGE_EVENT, callback)
+  addListener: callback => batteryEventEmitter.addListener(DeviceBattery.BATTERY_CHANGE_EVENT, callback),
+  removeListener: callback => batteryEventEmitter.removeListener(DeviceBattery.BATTERY_CHANGE_EVENT, callback)
 };
